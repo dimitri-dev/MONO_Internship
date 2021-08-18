3d2cfd8410483f84a5dab966deebab92fc88e52d
dimitri-dev <maric.neven03@gmail.com>
2021-08-19 00:05:05 +0200

    Day 3 - Baze podataka
    Dodani kontroleri za bazu podataka.

---
+++ _003 - Baze podataka/Controllers/CarsController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Web.Http;
+
+using _003___Baze_podataka.Models.Car;
+using _003___Baze_podataka.Repositories;
+
+namespace _003___Baze_podataka.Controllers
+{
+    [RoutePrefix("cars")]
+    public class CarsController : ApiController
+    {
+        private static ICarRepository _privateRepository = new CarRepository();
+
+        [HttpPost]
+        public IHttpActionResult Create([FromBody()] CreateCarDto dto)
+        {
+            if (dto == null) return BadRequest("Body empty.");
+
+            var item = _privateRepository.Create(dto);
+            return Content(System.Net.HttpStatusCode.Created, item);
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            var items = _privateRepository.GetAll();
+            return Ok(items);
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetById(Guid id)
+        {
+            Car item = _privateRepository.Get(id);
+
+            if (item == null) throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            return Ok(item);
+        }
+
+        [HttpPut]
+        public IHttpActionResult Update(Guid id, [FromBody] UpdateCarDto dto)
+        {
+            if (dto == null) return BadRequest("Body empty.");
+
+            Car item = _privateRepository.Update(id, dto);
+
+            if (item == null) throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            return Ok(item);
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Delete(Guid id)
+        {
+            try
+            {
+                _privateRepository.Delete(id);
+                return StatusCode(System.Net.HttpStatusCode.NoContent);
+            }
+            catch
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+        }
+    }
+}
---
+++ _003 - Baze podataka/Controllers/StudentsController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using _003___Baze_podataka.Repositories;
+using _003___Baze_podataka.Models.Student;
+using System.Net;
+
+namespace _003___Baze_podataka.Controllers
+{
+    [RoutePrefix("students")]
+    public class StudentsController : ApiController
+    {
+        private static IStudentRepository _privateRepository = new StudentRepository();
+
+        [HttpPost]
+        public IHttpActionResult Create([FromBody()] CreateStudentDto dto)
+        {
+            if (dto == null) return BadRequest("Body empty.");
+
+            var item = _privateRepository.Create(dto);
+            return Content(System.Net.HttpStatusCode.Created, item);
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            var items = _privateRepository.GetAll();
+            return Ok(items);
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetById(Guid id)
+        {
+            Student item = _privateRepository.Get(id);
+
+            if (item == null) throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            return Ok(item);
+        }
+
+        [HttpPut]
+        public IHttpActionResult Update(Guid id, [FromBody] UpdateStudentDto dto)
+        {
+            if (dto == null) return BadRequest("Body empty.");
+
+            Student item = _privateRepository.Update(id, dto);
+
+            if (item == null) throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            return Ok(item);
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Delete(Guid id)
+        {
+            try
+            {
+                _privateRepository.Delete(id);
+                return StatusCode(System.Net.HttpStatusCode.NoContent);
+            }
+            catch
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+        }
+    }
+}
