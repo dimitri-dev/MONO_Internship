948caa05abc218ab83b3f4da9c3b5a808104dcc7
dimitri-dev <maric.neven03@gmail.com>
2021-09-09 10:54:02 +0200

    Day 1 FE - React Components and States

--- _101 - Uvod u React/src/index.js
+++ _101 - Uvod u React/src/index.js
@@ -1,17 +1,82 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // regex from: https://stackoverflow.com/questions/48384163/javascript-remove-day-name-from-date
+      date: new Date().toDateString().replace(/^\S+\s/,''),
+      time: new Date().toLocaleTimeString()
+    };
+  }
+
+  componentDidMount() {
+    this.intervalID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date().toDateString().replace(/^\S+\s/,''),
+      time: new Date().toLocaleTimeString()
+    });
+  }
+
+  render() {
+    return (
+      <div className="text-center">
+        <h1>Današnji datum: {this.state.date} </h1>
+        <h2>Trenutno vrijeme: {this.state.time}.</h2>
+      </div>
+    );
+  }
+}
+
+class RandomNumberButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rand: null
+    };
+  }
+
+  handleClick = () => {
+    this.setState({
+      clicked: true, 
+      rand: (Math.floor(Math.random() * 10000) + 1) * (Math.random() < 0.5 ? -1 : 1)
+    });
+  }
+
+  render() {
+    return (
+      <div className="d-flex flex-column mt-5 justify-content-center align-items-center" style={{display:'flex'}}>
+          <button class="btn btn-primary" onClick={this.handleClick}>Give me a Random Integer</button>
+          <h1>{this.state.clicked && this.state.rand}</h1>
+      </div>
+    );
+  }
+}
+
+function App() {
+  return (
+    <div>
+      <Clock/>
+      <RandomNumberButton/>
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App/>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
